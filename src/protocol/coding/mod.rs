2da0feee13ee781c9136433027ddd558572b07a6
Lethe <lichenghaongyd@qq.com>
2025-08-04 15:25:07 +0000

    feat: wrapped raptorq encoder and decoder

--- src/protocol/coding/mod.rs
+++ src/protocol/coding/mod.rs
@@ -1,14 +1,13 @@
-use crate::protocol::constants::TRANSMISSION_INFO_LENGTH;
-
-pub trait FrameSender {
+pub trait FrameSender<const TRANSMISSION_INFO_LENGTH: usize> {
     fn init(chunk_data: &[u8], next_id: u32) -> Self;
     fn next_frame(&mut self) -> (u32, Vec<u8>);
     fn get_trasmission_info(&self) -> [u8; TRANSMISSION_INFO_LENGTH];
 }
 
-pub trait FrameReceiver: Sized {
-    fn try_init(frame: &[u8]) -> Option<Self>;
-    fn update(frame: &[u8]) -> Option<Vec<u8>>;
+pub trait FrameReceiver<const TRANSMISSION_INFO_LENGTH: usize>: Sized {
+    fn try_init(frame: &[u8; TRANSMISSION_INFO_LENGTH]) -> Option<Self>;
+    fn update(&mut self, frame_id: u32, frame: &[u8]) -> Option<Vec<u8>>;
+    fn expected_frame_id(&self) -> u32;
 }
 
 mod raptorq_code;
--- src/protocol/coding/raptorq_code.rs
+++ src/protocol/coding/raptorq_code.rs
@@ -1,7 +1,8 @@
-use crate::protocol::constants::DEFAULT_FRAME_LEN;
+use crate::protocol::{coding::FrameReceiver, constants::DEFAULT_FRAME_LEN};
 
 use super::FrameSender;
-use raptorq::{Encoder, ObjectTransmissionInformation};
+use crate::protocol::constants::TRANSMISSION_INFO_LENGTH as RAPTORQ_TRANSMISSION_INFO_LENGTH;
+use raptorq::{Decoder, Encoder, EncodingPacket, ObjectTransmissionInformation};
 
 use std::collections::VecDeque;
 
@@ -12,7 +13,7 @@ pub struct RaptorqSender {
     next_fetch_id: usize,
 }
 
-impl FrameSender for RaptorqSender {
+impl FrameSender<RAPTORQ_TRANSMISSION_INFO_LENGTH> for RaptorqSender {
     fn init(chunk_data: &[u8], next_id: u32) -> Self {
         let config = ObjectTransmissionInformation::with_defaults(
             chunk_data.len() as u64,
@@ -53,24 +54,51 @@ impl FrameSender for RaptorqSender {
         self.cache.pop_front().unwrap()
     }
 
-    fn get_trasmission_info(&self) -> [u8; crate::protocol::constants::TRANSMISSION_INFO_LENGTH] {
+    fn get_trasmission_info(&self) -> [u8; RAPTORQ_TRANSMISSION_INFO_LENGTH] {
         self.encoder.get_config().serialize()
+    }
+}
+
+pub struct RaptorqReceiver {
+    decoder: Decoder,
+    expected_frame_id: u32,
+}
+
+impl FrameReceiver<RAPTORQ_TRANSMISSION_INFO_LENGTH> for RaptorqReceiver {
+    fn try_init(frame: &[u8; RAPTORQ_TRANSMISSION_INFO_LENGTH]) -> Option<Self> {
+        let config = ObjectTransmissionInformation::deserialize(frame);
+        let decoder = Decoder::new(config);
+        Self {
+            decoder,
+            expected_frame_id: 0,
+        }
+        .into()
+    }
+    fn update(&mut self, frame_id: u32, frame: &[u8]) -> Option<Vec<u8>> {
+        self.expected_frame_id = self.expected_frame_id.max(frame_id + 1);
+        self.decoder.decode(EncodingPacket::deserialize(frame))
+    }
+    fn expected_frame_id(&self) -> u32 {
+        self.expected_frame_id
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    const FILESIZE: usize = 1048576;
+    const CHUNK_SIZE: usize = 1048576;
     use rand::Rng;
 
     use crate::protocol::{
-        coding::{FrameSender, raptorq_code::RaptorqSender},
+        coding::{
+            FrameReceiver, FrameSender,
+            raptorq_code::{RaptorqReceiver, RaptorqSender},
+        },
         constants::MTU,
     };
 
-    fn generate_random() -> Vec<u8> {
-        let mut data: Vec<u8> = vec![0; FILESIZE];
+    fn generate_random(size: usize) -> Vec<u8> {
+        let mut data: Vec<u8> = vec![0; size];
         for byte in data.iter_mut() {
             *byte = rand::rng().random();
         }
@@ -79,7 +107,7 @@ mod test {
 
     #[test]
     fn get_gen_frames() {
-        let data = generate_random();
+        let data = generate_random(CHUNK_SIZE);
         let mut generator = RaptorqSender::init(&data, 64);
         for (i, (j, data)) in std::iter::from_fn(|| generator.next_frame().into())
             .enumerate()
@@ -88,5 +116,36 @@ mod test {
             assert_eq!(i + 64, j as usize);
             assert!(data.len() <= MTU);
         }
+    }
+
+    #[test]
+    fn decoding() {
+        let data = generate_random(CHUNK_SIZE);
+        let mut encoder = RaptorqSender::init(&data, 0);
+
+        let config = encoder.get_trasmission_info();
+        let mut decoder = RaptorqReceiver::try_init(&config).unwrap();
+
+        for i in 0..600 {
+            let (frame_id, frame) = encoder.next_frame();
+            if i % 5 != 0 {
+                decoder.update(frame_id, &frame);
+            }
+        }
+
+        // Mock a restart
+
+        let restart_id = decoder.expected_frame_id();
+        let mut encoder = RaptorqSender::init(&data, restart_id);
+
+        let restored_data = loop {
+            let (frame_id, frame) = encoder.next_frame();
+            assert!(frame_id < 1000, "Take too long!");
+            if let Some(restored_data) = decoder.update(frame_id, &frame) {
+                break restored_data;
+            }
+        };
+
+        assert_eq!(data, restored_data);
     }
 }
